2c1951d60ac73457ca593641a1a856b8d50427f8
composercordero <composercordero@gmail.com>
2023-07-12 10:48:59 -0500

    JS planning

---
+++ script.js
@@ -0,0 +1,25 @@
+// variables
+
+const email = document.getElementById("user");
+const password = document.getElementById("password");
+const buttonSignIn = document.getElementById("signIn");
+const formInput = document.querySelector('.formInput');
+
+// Click
+
+buttonSignIn.addEventListener(click, validateUser);
+
+// functions
+
+//Email Validation
+
+
+//Password Validation
+
+// function validateUser(){
+//     if(true){
+//         formInput.classList.add('valid');
+//     }else{
+//         formInput.classList.add('invalid');
+// }
+// }
